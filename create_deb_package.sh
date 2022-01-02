8061677a3e9e3744f977618d0fca08a79fce556a
Pete Warden <pete@petewarden.com>
2022-01-01 18:07:45 -0800

    Fixed binary copying problem in package creator

--- create_deb_package.sh
+++ create_deb_package.sh
@@ -12,9 +12,9 @@ LIB_URL="https://github.com/coqui-ai/STT/releases/download/v1.1.0/native_client.
 LIB_TMP_DIR=${BUILD_DIR}lib_tmp/
 LIB_DIR=${BUILD_DIR}lib/
 DEB_DIR=${BUILD_DIR}${NAME}_${VERSION}_${ARCH}/
-DEB_MODELS_DIR=${DEB_DIR}/etc/spchcat/models
-DEB_LIB_DIR=${DEB_DIR}/usr/lib
-DEB_BIN_DIR=${DEB_DIR}/usr/bin
+DEB_MODELS_DIR=${DEB_DIR}/etc/spchcat/models/
+DEB_LIB_DIR=${DEB_DIR}/usr/lib/
+DEB_BIN_DIR=${DEB_DIR}/usr/bin/
 DEB_DEBIAN_DIR=${DEB_DIR}DEBIAN/
 DEB_CONTROL_FILE=${DEB_DEBIAN_DIR}control
 
@@ -40,6 +40,7 @@ mkdir -p ${DEB_LIB_DIR}
 cp -r ${LIB_DIR}*.so ${DEB_LIB_DIR}
 
 # Copy over the binary executable.
+mkdir -p ${DEB_BIN_DIR}
 cp -r ${NAME} ${DEB_BIN_DIR}
 
 # Set up the metadata for the package.
