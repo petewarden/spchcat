b547dbf0cab5f7ccc2e26e3fe05e3d56906dfcdd
Pete Warden <pete@petewarden.com>
2022-01-02 15:21:25 -0800

    Updated package builder

--- create_deb_package.sh
+++ create_deb_package.sh
@@ -13,7 +13,7 @@ LIB_TMP_DIR=${BUILD_DIR}lib_tmp/
 LIB_DIR=${BUILD_DIR}lib/
 DEB_DIR=${BUILD_DIR}${NAME}_${VERSION}_${ARCH}/
 DEB_MODELS_DIR=${DEB_DIR}/etc/spchcat/models/
-DEB_LIB_DIR=${DEB_DIR}/usr/local/lib/
+DEB_LIB_DIR=${DEB_DIR}/usr/lib/
 DEB_BIN_DIR=${DEB_DIR}/usr/local/bin/
 DEB_DEBIAN_DIR=${DEB_DIR}DEBIAN/
 DEB_CONTROL_FILE=${DEB_DEBIAN_DIR}control
@@ -23,7 +23,7 @@ rm -rf ${DEB_DIR}
 # Copy over the language models. You'll need to run the download_releases.py
 # first to fetch the required files.
 mkdir -p ${DEB_MODELS_DIR}
-cp -r ${MODELS_DIR}* ${DEB_MODELS_DIR}
+cp -r --verbose ${MODELS_DIR}* ${DEB_MODELS_DIR}
 # Remove the TensorFlow model graphs, since these are only used for training
 # and are quite large.
 find ${DEB_MODELS_DIR} -type f -name "*.pb*" -delete
@@ -37,11 +37,11 @@ unxz ${LIB_TMP_DIR}native_client.tflite.Linux.tar.xz
 rm -rf ${LIB_DIR}  && mkdir -p ${LIB_DIR}
 tar -xf ${LIB_TMP_DIR}native_client.tflite.Linux.tar --directory ${LIB_DIR}
 mkdir -p ${DEB_LIB_DIR}
-cp -r ${LIB_DIR}*.so ${DEB_LIB_DIR}
+cp -r  --verbose ${LIB_DIR}*.so ${DEB_LIB_DIR}
 
 # Copy over the binary executable.
 mkdir -p ${DEB_BIN_DIR}
-cp -r ${NAME} ${DEB_BIN_DIR}
+cp -r --verbose ${NAME} ${DEB_BIN_DIR}
 
 # Set up the metadata for the package.
 mkdir -p ${DEB_DEBIAN_DIR}
